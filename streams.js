dcd442db2f3d49889ca9c2385a5e506450338389
Sam Zudell <s.zudell98@gmail.com>
2023-04-15 19:46:13 -0400

    fixed typo on streams.js

--- streams.js
+++ streams.js
@@ -5,19 +5,19 @@ const { createServer } = require("http");
 const port = 3000;
 
 const server = createServer((req, res) => {
-  const { url, method } = req;
+    const { url, method } = req;
 
-  if (url === "/" && method === "GET") {
-    res.setHeader("Content-Type", "text/html");
-    const readStream = createReadStream(join(__dirname, "./public/index.html"));
-    readStream.pipe(res);
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    const readStream = createReadStream(
-      join(__dirname, "./public/notFound404.html")
-    );
-    readStream.pipe(res);
-  }
+    if (url === "/" && method === "GET") {
+        res.setHeader("Content-Type", "text/html");
+        const readStream = createReadStream(join(__dirname, "./public/index.html"));
+        readStream.pipe(res);
+    } else {
+        res.setHeader("Content-Type", "text/html");
+        const readStream = createReadStream(
+            join(__dirname, "./public/notFound.html")
+        );
+        readStream.pipe(res);
+    }
 });
 
 server.listen(port, () => console.log("Server listening on " + port + "..."));
